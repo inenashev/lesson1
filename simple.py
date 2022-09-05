62d6a68c8953e707a7b85056eda73e9f68e4773c
Ivan Nenashev <inenashev@icloud.com>
2022-09-05 14:28:11 +0300

    simple py

--- simple.py
+++ simple.py
@@ -11,3 +11,10 @@ print(int(v) + 10)
 
 name = input("Введите ваше имя: ")
 print(f'Привет, {name}! Как дела?')
+
+
+print(float('1'))
+print(int('2.5'))
+print(bool(1))
+print(bool(""))
+print(bool(0))
